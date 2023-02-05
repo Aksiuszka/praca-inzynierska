f6b809aca7d462f390c55c158594f8ed3978f93d
Aksiuszka <70382984+Aksiuszka@users.noreply.github.com>
2023-02-05 19:51:57 +0100

    configuration/muiStyled configured Mui and Styled (#8)

--- frontend/src/shared/components/Test/Test.jsx
+++ frontend/src/shared/components/Test/Test.jsx
@@ -1,6 +1,13 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { styled } from '@mui/material/styles';
+import { Div2 } from './styled';
 
-function Test() {
+export const Div = styled('div')`
+  width: 10px;
+  height: 10px;
+  border: 2px solid red;
+`;
+const Test = () => {
   const [counter, setCounter] = useState(0);
   const add = () => {
     return setCounter(counter + 1);
@@ -8,10 +15,14 @@ function Test() {
   return (
     <>
       <p>Hello React</p>
+      <Div />
+      <Div2 />
       <div>{counter}</div>
-      <button onClick={add}>add</button>
+      <button type='button' onClick={add}>
+        add
+      </button>
     </>
   );
-}
+};
 
 export default Test;
