72f1f067c8dd66372a5440c36190da3599eb83d3
Anna Kawecka <aakawecka89@gmail.com>
2023-02-05 19:50:00 +0100

    configuration/muiStyled configured Mui and Styled

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,10 @@
+import Test from './shared/components/Test/Test';
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Test />
+    </div>
+  );
 }
 
 export default App;
