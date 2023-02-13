0cfd6d5c6f84042f40ebacc7fbe16e954fc8bd0f
Anna Kawecka <aakawecka89@gmail.com>
2023-02-13 18:01:18 +0100

    feature/layout/container added layout container

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,18 @@ import { ROUTES } from './shared/constants';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import About from './pages/About';
+import Layout from './shared/components/Layout';
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path={ROUTES.root} element={<Home />} />
-        <Route path={ROUTES.dashboard} element={<Dashboard />} />
-        <Route path={ROUTES.about} element={<About />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path={ROUTES.root} element={<Home />} />
+          <Route path={ROUTES.dashboard} element={<Dashboard />} />
+          <Route path={ROUTES.about} element={<About />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 };
---
+++ frontend/src/shared/components/Layout/index.jsx
@@ -0,0 +1,37 @@
+import { Grid } from '@mui/material';
+import Footer from '../Footer';
+import Header from '../Header';
+
+const Layout = ({ children }) => {
+  return (
+    <Grid
+      sx={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}
+    >
+      <Grid container item justifyContent='center'>
+        <Header />
+        <Grid
+          container
+          justifyContent='center'
+          sx={{
+            padding: (theme) => {
+              return {
+                md: theme.spacing(10),
+                xs: theme.spacing(4, 1.2, 1.2),
+              };
+            },
+            width: '100%',
+          }}
+        >
+          {children}
+        </Grid>
+      </Grid>
+      <Footer />
+    </Grid>
+  );
+};
+export default Layout;
