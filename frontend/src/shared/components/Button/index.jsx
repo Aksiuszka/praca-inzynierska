0b097d1865f8e1e4614a9b5a1e521108ec36884d
Anna Kawecka <aakawecka89@gmail.com>
2023-02-14 18:11:44 +0100

    feature/button added button component

---
+++ frontend/src/shared/components/Button/index.jsx
@@ -0,0 +1,42 @@
+import {
+  CustomBtn as Button,
+  CustomTypography as Typography,
+  AccentBtn,
+  AccenyTypography,
+} from './style';
+
+const CustomButton = ({
+  variant,
+  label,
+  color,
+  size,
+  sx,
+  className,
+  onClick,
+  disableFocusRipple,
+  ...props
+}) => {
+  if (variant === 'regular') {
+    return (
+      <Button
+        color={color}
+        size={size}
+        disableFocusRipple={disableFocusRipple}
+        onClick={onClick}
+        {...props}
+      >
+        <Typography className={className} sx={sx?.label}>
+          {label}
+        </Typography>
+      </Button>
+    );
+  }
+  return (
+    <AccentBtn color={color} size={size} disableFocusRipple={disableFocusRipple} {...props}>
+      <AccenyTypography className={className} sx={sx?.label}>
+        {label}
+      </AccenyTypography>
+    </AccentBtn>
+  );
+};
+export default CustomButton;
