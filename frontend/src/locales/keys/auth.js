9330bdc2b30afee64a920ea3f7c04e8319e698a8
Anna Kawecka <aakawecka89@gmail.com>
2023-04-22 22:35:50 +0200

    feature/ refactor of locales in auth

---
+++ frontend/src/locales/keys/auth.js
@@ -0,0 +1,15 @@
+export default {
+  headline: {
+    login: 'auth.headline.login',
+  },
+  input: {
+    username: 'auth.input.username',
+    pass: 'auth.input.pass',
+  },
+  button: {
+    login: 'auth.button.login',
+  },
+  google: {
+    btn: 'auth.google.btn',
+  },
+};
