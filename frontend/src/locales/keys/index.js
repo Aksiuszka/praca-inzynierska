c341051fe8355d609478a5e90ac5215679dde85e
Aksiuszka <70382984+Aksiuszka@users.noreply.github.com>
2023-05-21 14:02:28 +0200

    Feature/login UI (#34)
    * feature/login-ui added login ui and sm comp
    
    * feature/ refactor of locales in auth
    
    * feature / refactor of auth translations

--- frontend/src/locales/keys/index.js
+++ frontend/src/locales/keys/index.js
@@ -1,9 +1,11 @@
 import commonKeys from './common';
 import homeKeys from './home';
 import aboutKeys from './about';
+import authKeys from './auth';
 
 export default {
   common: commonKeys,
   home: homeKeys,
   about: aboutKeys,
+  auth: authKeys,
 };
--- frontend/src/locales/translations/pl/index.js
+++ frontend/src/locales/translations/pl/index.js
@@ -1,11 +1,13 @@
 import common from './common';
 import home from './home';
 import about from './about';
+import auth from './auth';
 
 const translations = {
   ...common,
   ...home,
   ...about,
+  ...auth,
 };
 
 export default { translations };
