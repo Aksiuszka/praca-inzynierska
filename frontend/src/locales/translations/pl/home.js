b7e5a3130c97eb3c57928c13a932e0636c96a0ce
Aksiuszka <70382984+Aksiuszka@users.noreply.github.com>
2024-01-21 14:20:10 +0100

    Feat/profile page (#59)
    * feat/result basic structure
    
    * feat/assets
    
    * hsdghdfds
    
    * result page UI for smartTest and routing
    
    * feat/basic prescreening structure
    
    * Prescreen results, refactor needed
    
    * test result UI
    
    * basic structure and routing of profile
    
    * about refactor
    
    * contact email sending logic
    
    * added content and link

--- frontend/src/locales/translations/pl/home.js
+++ frontend/src/locales/translations/pl/home.js
@@ -4,12 +4,12 @@ export default {
   // Hero
   [keys.home.hero.main]: 'Get Yourself',
   [keys.home.hero.mainAccent]: 'A Friend',
-  [keys.home.hero.subtext]: 'Subtext concerning the app',
-  [keys.home.hero.btn]: 'start the process',
+  [keys.home.hero.subtext]: 'Find Your Fluff: Unleash Joy through Pet Adoption!',
+  [keys.home.hero.btn]: 'zacznij proces',
 
   // headlines
-  [keys.home.headline.hero]: 'Lorem ipsum dolor sit amet, cotetur',
-  [keys.home.headline.heroAccent]: 'adipiscing.',
+  [keys.home.headline.hero]: 'Czy wiesz, dlaczego fluff jest liderem w',
+  [keys.home.headline.heroAccent]: 'adopcjach?',
   [keys.home.headline.faq]: 'Get to know us',
   [keys.home.headline.faqAccent]: 'more',
   [keys.home.headline.slider]: 'What our users ',
@@ -18,27 +18,27 @@ export default {
   // cards
   [keys.home.card.yellow.title]: '10k + users',
   [keys.home.card.yellow.content]:
-    'Lorem ipsum dolor sit amet, cotetur adipiscing. Lorem ipsum dolor sit amet, cotetur adipiscing.',
+    ' Razem tworzymy rodzinę, gotową do dzielenia radości adopcji i nieskończonej miłości do naszych futrzastych przyjaciół.',
   [keys.home.card.orange.title]: 'Perfect Match',
   [keys.home.card.orange.content]:
-    'Lorem ipsum dolor sit amet, cotetur adipiscing. Lorem ipsum dolor sit amet, cotetur adipiscing.',
+    ' W Fluff każde zwierzę znajduje swoje idealne dopasowanie, tworząc niezapomniane relacje w pełni zasługujące na miano Perfect Match.',
   [keys.home.card.pink.title]: 'Smart AI',
   [keys.home.card.pink.content]:
-    'Lorem ipsum dolor sit amet, cotetur adipiscing. Lorem ipsum dolor sit amet, cotetur adipiscing.',
+    'Dzięki AI każde zwierzę odnajduje swoje inteligentne dopasowanie, zapewniając nie tylko radość, ale także mądrą i harmonijną więź między ludźmi a zwierzętami.',
 
   // faq
   [keys.home.faq.one.number]: '01 ',
   [keys.home.faq.one.title]: 'How can I create an account?',
   [keys.home.faq.one.content]:
-    '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    'Aby utworzyć konto, wejdź na naszą stronę internetową i wybierz opcję "Zarejestruj się". Wypełnij krótki formularz rejestracyjny, podając podstawowe informacje, takie jak adres e-mail i hasło. Kliknij "Zarejestruj się" i oczekuj na potwierdzenie rejestracji. Po tym prostym procesie Twoje konto będzie gotowe do użytku.',
   [keys.home.faq.two.number]: '02 ',
   [keys.home.faq.two.title]: 'Is this app free?',
   [keys.home.faq.two.content]:
-    '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    ' Bez żadnych ukrytych opłat czy subskrypcji, Fluff zapewnia łatwy dostęp do wszystkich funkcji bez ponoszenia kosztów. Dzięki temu każdy może cieszyć się pełnym zakresem korzyści, jakie oferuje nasza aplikacja.',
   [keys.home.faq.three.number]: '03 ',
   [keys.home.faq.three.title]: 'What countries is fluff available in?',
   [keys.home.faq.three.content]:
-    '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    'Fluff obecnie jest dostępny wyłącznie w Polsce. Jesteśmy zaszczyceni, że możemy dostarczać nasze usługi miłośnikom zwierząt, tworząc dla nich wyjątkowe doświadczenia związane z adopcją. Mimo że obecnie jesteśmy dostępni tylko w Polsce, nieustannie pracujemy nad poszerzeniem naszego zasięgu.',
 
   // call to action
   [keys.home.cta.subtext.first]: 'Ready to ',
