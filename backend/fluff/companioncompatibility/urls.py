215ac9eefdb43f30a9100319e3dcad5eb319812a
Giovanna-89 <83236088+Giovanna-89@users.noreply.github.com>
2024-01-30 19:30:57 +0100

    Feature/smart test (#64)
    * feature/smart-test startapp/model-improvements
    
    * feature/smart-test add serializers
    
    * feature/smart-test add views
    
    * feature/smart-test add get methods
    
    * feature/smart-test optimize temperament counting
    
    * feature/smart-test PersonTest
    
    * fix models
    
    * feature/smart-test prescreening
    
    * feature/smart-test models
    
    * feature/smart-test companioncompatibility
    
    * feature/smart-test fix yml

---
+++ backend/fluff/companioncompatibility/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from companioncompatibility import views
+
+router = DefaultRouter()
+router.register("smart-test-answers", views.SmartTestAnswerViewSet)
+router.register("smart-test-results", views.SmartTestResultViewSet)
+router.register("smart-test-dog", views.DogPersonaltyQuestionViewSet)
+router.register("smart-test-dog-results", views.DogPersonalityResultViewSet)
+router.register("prescreening-questions", views.PrescreeningViewSet)
+
+
+app_name = "companioncompatibility"
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
