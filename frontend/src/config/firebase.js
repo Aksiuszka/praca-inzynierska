c16ef90cf4380d4a770528597fa67b6ec40111c7
Aksiuszka <70382984+Aksiuszka@users.noreply.github.com>
2024-02-11 14:10:39 +0100

    rbac via firebase (#67)

--- frontend/src/config/firebase.js
+++ frontend/src/config/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBB1w53vqOzDjYreV4cDfYIwKGHDEE5Hb8',
@@ -14,5 +15,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider(auth);
+const db = getFirestore();
 
-export { auth, provider };
+export { auth, provider, db };
