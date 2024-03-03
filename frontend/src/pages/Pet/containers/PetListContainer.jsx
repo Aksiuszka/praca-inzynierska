955d21c7d4c9d6c7fa3971cce072bfbd08ce284a
Aksiuszka <70382984+Aksiuszka@users.noreply.github.com>
2024-03-03 13:28:16 +0100

    Feat/logic pet profile (#81)
    * fix/added email structure, fixed textfield for pet profile
    
    * feat/pet display using id, no be yet

--- frontend/src/pages/Pet/containers/PetListContainer.jsx
+++ frontend/src/pages/Pet/containers/PetListContainer.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-array-index-key */
 /* eslint-disable no-unused-vars */
 import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
@@ -6,6 +7,7 @@ import { Typography } from '@mui/material';
 import { db } from '../../../config/firebase';
 import { PinkCard } from '../styles';
 import { PetListItem } from '../components/PetListItem/PetListItem';
+import { petArr } from '../mock/mockData';
 
 const blobToUrl = (blobData) => {
   if (blobData instanceof Blob) {
@@ -38,11 +40,17 @@ export const PetListContainer = () => {
   return (
     <PinkCard>
       <Typography variant='decoratedSmall'>Twoi podopieczni</Typography>
-      {petList ? (
-        <PetListItem name={petList.name} town={petList.town} img={blobToUrl(blobUrl)} />
-      ) : (
-        'Nie masz dodanych podopiecznych'
-      )}
+      {petArr.length > 0
+        ? petArr.map((pet, index) => (
+            <PetListItem
+              key={`${pet.name}-${index}`}
+              id={`${pet.name}-${index}`}
+              name={pet.name}
+              town={pet.town}
+              img={blobToUrl(pet.blobUrl)}
+            />
+          ))
+        : 'Nie masz dodanych podopiecznych'}
     </PinkCard>
   );
 };
